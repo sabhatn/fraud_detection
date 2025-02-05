206abca2226ed4069bd7a2a2ade8069c6726ccc2
sabhatn <sachin.coolgemini@gmail.com>
2025-02-05 14:26:20 +0530

    added 3 ml models

--- ml_model.py
+++ ml_model.py
@@ -2,3 +2,11 @@
 
 print("adding all the code related to machine learning model")
 
+def ml_model1():
+    print("machine learning model 1")
+
+def ml_model2():
+    print("machine learning model 2")
+
+def ml_model3():
+    print("machine learning model 3")
